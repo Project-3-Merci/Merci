54ae2ee9a5c417c57d9e5b7c9997079abac3db1b
Carwi Barrios <carwelsbarrios@gmail.com>
2022-03-11 10:52:23 +0100

    map API

--- Project3-Client/src/pages/CreateFavourPage.js
+++ Project3-Client/src/pages/CreateFavourPage.js
@@ -3,11 +3,16 @@ import IsAnon from "../components/IsAnon";
 import { Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
+import Map from "../components/Map"
 
 export default function CreateFavourPage() {
     const { isLoggedIn } = useContext(AuthContext);
     if(isLoggedIn){
-        return <div>CREATE NEW FAVOUR PAGE</div>
+        return (<div>
+        <h1>CREATE NEW FAVOUR PAGE</h1>
+        {/* <Map /> */}
+        </div>)
+       
      }
      else{
          return <Navigate to="/login"></Navigate>
