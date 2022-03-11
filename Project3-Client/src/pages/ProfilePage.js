017facdd31c5bdea456610d224231bbca722fc79
Luis Moncada <raketentrakt0r@gmail.com>
2022-03-11 13:08:19 +0100

    Addprops

--- Project3-Client/src/pages/ProfilePage.js
+++ Project3-Client/src/pages/ProfilePage.js
@@ -1,4 +1,4 @@
-import { useContext} from "react";
+import { useContext, useEffect, useState} from "react";
 import { AuthContext } from "../context/auth.context";
 import { Navigate } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -6,18 +6,24 @@ import { Link } from "react-router-dom";
 
 export default function ProfilePage(props){
 
-    const { isLoggedIn, user} = useContext(AuthContext)
+    const [profile, setProfile] = useState([])
+
+    useEffect(()=>{
+        setProfile(props.profile)
+    },[props.profile])
+
+    const { isLoggedIn} = useContext(AuthContext)
     if(isLoggedIn){
         return (
         <div>
 
         <h1>Profile Page</h1>
-        <img src=""/>
-        <h2>{user.name}</h2>
-        <h3>{user.email}</h3>
-        <h4>Age:</h4>
+        <img src={profile.profileImg} alt="Profile Image"/>
+        <h2>{profile.name}</h2>
+        <h3>Age: {profile.age}</h3>
+        <h3>Abot me: {profile.aboutMe}</h3>
         <h4>About me:</h4>
-        
+
         <Link to={"/profile/:id"}> <button >Edit</button></Link>
 
         </div>)
