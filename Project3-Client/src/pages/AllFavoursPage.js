de205566c4f26e870487d6a430039ebeadb77bae
Luis Moncada <raketentrakt0r@gmail.com>
2022-03-14 15:54:37 +0100

    bootstrap

--- Project3-Client/src/pages/AllFavoursPage.js
+++ Project3-Client/src/pages/AllFavoursPage.js
@@ -1,23 +1,20 @@
 import { useState, useEffect, useContext } from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 import apiService from "../services/api.service";
 
-
 function AllFavoursPage() {
   const [favours, setFavours] = useState([]);
-  const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
 
-  console.log("USER:", user)
+  console.log("USER:", user);
   const getAllFavours = () => {
     // Get the token from the localStorage
     const storedToken = localStorage.getItem("authToken");
 
     // Send the token through the request "Authorization" Headers
     apiService
-      .getAll(
-        "favours"
-      )
+      .getAll("favours")
       .then((response) => setFavours(response.data))
       .catch((error) => console.log(error));
   };
@@ -30,38 +27,35 @@ function AllFavoursPage() {
 
   return (
     <div>
-
       <h1>All Favours Page</h1>
 
       {favours.map((favour) => {
-        console.log(favour)
+        console.log(favour);
 
-        if (!favour.taker && (favour.asker._id !== user?._id))
+        if (!favour.taker && favour.asker._id !== user?._id)
           return (
-
             <div className="favour-preview card">
-                <h2>{favour.title}</h2>
-                <h4>{favour.asker.name}</h4>
-                <Link to={`/favour/${favour._id}`}>
-                    <button>See details</button>
-                </Link>
-              {user?._id && (<button onClick={()=>{
-                    apiService.updateOne(`favours/${user._id}/accept`, favour._id,{})
-                    .then(()=>getAllFavours())
-                    }}>Accept</button>)}
+              <h2>{favour.title}</h2>
+              <h4>{favour.asker.name}</h4>
+              <Link to={`/favour/${favour._id}`}>
+                <button>See details</button>
+              </Link>
+              {user?._id && (
+                <button
+                  onClick={() => {
+                    apiService
+                      .updateOne(`favours/${user._id}/accept`, favour._id, {})
+                      .then(() => getAllFavours());
+                  }}
+                >
+                  Accept
+                </button>
+              )}
             </div>
-
-
-          )
-      }
-
-
-
-      )}
-
+          );
+      })}
     </div>
   );
 }
 
 export default AllFavoursPage;
-
--- Project3-Client/src/pages/EditProfilePage.js
+++ Project3-Client/src/pages/EditProfilePage.js
@@ -2,8 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import apiService from "../services/api.service";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function EditProfilePage(props) {
   const [profile, setProfile] = useState([]);
@@ -53,9 +52,8 @@ function EditProfilePage(props) {
     e.preventDefault();
 
     // Get the token from the localStorage
-    apiService.updateOne("profile/edit", id, formData)
-    .then(() => {
-       navigate(`/profile/${id}`)
+    apiService.updateOne("profile/edit", id, formData).then(() => {
+      navigate(`/profile/${id}`);
     });
   };
 
@@ -64,9 +62,14 @@ function EditProfilePage(props) {
       <h3 className="text-info">Edit Profile</h3>
 
       <form onSubmit={handleFormSubmit}>
-      <label className="p-2 text-center">Image</label>
+        <label className="p-2 text-center">Image</label>
         <div className="p-2">
-          <img  className="rounded img-fluid" src={photoUrl} alt="profile image" width={80} />
+          <img
+            className="rounded img-fluid"
+            src={photoUrl}
+            alt="profile image"
+            width={80}
+          />
           <input
             type="file"
             onChange={(e) => setImageSelected(e.target.files[0])}
@@ -74,37 +77,47 @@ function EditProfilePage(props) {
         </div>
 
         <div>
-        <button className="btn btn-danger mx-auto" type="button" onClick={uploadImage}>
-          Upload Image
-        </button>
+          <button
+            className="btn btn-danger mx-auto"
+            type="button"
+            onClick={uploadImage}
+          >
+            Upload Image
+          </button>
         </div>
-      
+
         <label className="text-center">Age:</label>
-        <div  className="mx-auto">
-        <input className="rounded"
-          type="number"
-          name="age"
-          min="0"
-          value={formData.age}
-          onChange={(e) => {
-            if (e.target.value < 18) e.target.value = 18;
-            handleChange(e);
-          }}
-        />
+        <div className="mx-auto">
+          <input
+            className="rounded"
+            type="number"
+            name="age"
+            min="0"
+            value={formData.age}
+            onChange={(e) => {
+              if (e.target.value < 18) e.target.value = 18;
+              handleChange(e);
+            }}
+          />
         </div>
-        
+
         <label className="text-center">About me:</label>
-          <div className="p-2">
-        <textarea className="text-dark alert alert-info border border-info"
-          name="aboutMe"
-          onChange={handleChange}
-          value={formData.aboutMe}
-        ></textarea>
-          </div>
         <div className="p-2">
-        <button className="btn btn-outline-success" type="submit" onClick={() => (formData.profileImg = photoUrl)}>
-          Save Changes
-        </button>
+          <textarea
+            className="text-dark alert alert-info border border-info"
+            name="aboutMe"
+            onChange={handleChange}
+            value={formData.aboutMe}
+          ></textarea>
+        </div>
+        <div className="p-2">
+          <button
+            className="btn btn-outline-success"
+            type="submit"
+            onClick={() => (formData.profileImg = photoUrl)}
+          >
+            Save Changes
+          </button>
         </div>
       </form>
     </div>
