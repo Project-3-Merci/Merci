e86c97be027e8ebf379a6e7828be709a238b935e
Diego Yupanqui <diegoyupanquichavarry@gmail.com>
2022-03-15 18:30:12 +0100

    chat

--- Project3-Client/src/pages/ChatPage.js
+++ Project3-Client/src/pages/ChatPage.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState, useContext} from "react";
+import { useState, useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
@@ -14,71 +14,75 @@ export default function ChatList() {
     const [receiver, setReceiver] = useState({})
     const [chat, setChat] = useState({})
 
-    const {id, otherId} = useParams()
-  
+    const { id, otherId } = useParams()
+
     const getAllMessages = () => {
 
-      // Send the token through the request "Authorization" Headers
-      apiService
-        .getAll(`chats/${id}/${otherId}`)
-        .then((response) => {
-            setReceiver(response.data.user1._id == id ? response.data.user2: response.data.user1) 
-            setMessages(response.data.messagess)
-            setChat(response.data)
-        })
-        .catch((error) => console.log(error));
+        // Send the token through the request "Authorization" Headers
+        apiService
+            .getAll(`chats/${id}/${otherId}`)
+            .then((response) => {
+                setReceiver(response.data.user1._id == id ? response.data.user2 : response.data.user1)
+                setMessages(response.data.messagess)
+                setChat(response.data)
+            })
+            .catch((error) => console.log(error));
     };
-  
+
     // We set this effect will run only once, after the initial render
     // by setting the empty dependency array - []
     useEffect(() => {
-      getAllMessages();
+        getAllMessages();
     }, []);
 
 
     useEffect(() => {
-        socket.on('updateChat', users =>{
-            if(users.includes(id)){
+        socket.on('updateChat', users => {
+            if (users.includes(id)) {
                 getAllMessages()
                 console.log("Chat updated")
             }
         })
-      }, []);
-  
+    }, []);
 
-      
-    const addNewMessage= () => {
-        apiService.createOne(`chats/newMessage/${chat._id}`, {content: newMessage, sender: id, receiver: otherId})
-        .then(response=>{
-            socket.emit('newMessage', [response.data.sender, response.data.receiver])
-        })
+
+
+    const addNewMessage = () => {
+        apiService.createOne(`chats/newMessage/${chat._id}`, { content: newMessage, sender: id, receiver: otherId })
+            .then(response => {
+                socket.emit('newMessage', [response.data.sender, response.data.receiver])
+            })
     }
-//    if(messages.length)
-    return (
-      <div className="chat-box">
 
-        <h2>{receiver.name}</h2>
+    if (messages.length > 0)
+        return (
+            <div className="chat-box">
 
-        {messages.map((message) => {
+                <h2>{receiver.name}</h2>
 
-            return (
-                <p>{message.content}</p>
-            )
-        },
+                {messages.map((message) => {
+                    const messageDate = new Date(message.createdAt)
+                    const daysPassed = Math.floor((new Date().getTime() - messageDate.getTime()) / (1000 * 3600 * 24))
+                    const messageFooter = daysPassed > 0 ?
+                    return (
+                        <div className="my-message-div">
+                            <p className="message-content">{message.content}</p>
+                            <p className="message-date">{daysPassed}</p>
+                        </div>
+                    )
+                },
 
-        
-        )}
-        <textarea onChange={e=>setNewMessage(e.target.value)}></textarea>
-            <button type="button" onClick={addNewMessage}>Send</button>
-      </div>
-    );
-  //  else
-{/*    return(
-        <div>
-            <h2>Start a conversation with {receiver.name}</h2>
-            <textarea onChange={e=>setNewMessage(e.target.value)}></textarea>
-            <button type="button" onClick={addNewMessage}>Send</button>
-        </div>
-        
-)*/}
-  }
+                )}
+                <textarea onChange={e => setNewMessage(e.target.value)}></textarea>
+                <button type="button" onClick={addNewMessage}>Send</button>
+            </div>
+        );
+    else
+        return (
+            <div>
+                <h2>Start a conversation with {receiver.name}</h2>
+                <textarea onChange={e => setNewMessage(e.target.value)}></textarea>
+                <button type="button" onClick={addNewMessage}>Send</button>
+            </div>
+        )
+}
--- Project3-Server/models/Message.model.js
+++ Project3-Server/models/Message.model.js
@@ -5,6 +5,8 @@ const messageSchema = new Schema({
   content: { type: String },
   sender: { type: Schema.Types.ObjectId, ref:"user" },
   receiver: { type: Schema.Types.ObjectId, ref:"user"}
+},{
+  timestamps:true
 });
 
 module.exports = model("Message", messageSchema);
