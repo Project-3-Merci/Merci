5cc79b09e1fe0df20b8c134bea661f0f96f305a5
Luis Moncada <raketentrakt0r@gmail.com>
2022-03-16 12:16:52 +0100

    no bootstrap

--- Project3-Client/src/index.js
+++ Project3-Client/src/index.js
@@ -4,8 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min'
+
 
 import { AuthProviderWrapper } from "./context/auth.context"; // <== IMPORT
 
