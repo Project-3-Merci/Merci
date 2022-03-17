3c2fb7f1bac382e4818c727a2689fc9fe97cbf08
Diego Yupanqui <76239228+DiegoYupanqui98@users.noreply.github.com>
2022-03-17 14:27:50 +0100

    files updated

---
+++ server.js
@@ -0,0 +1,29 @@
+const app = require("./app")
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+  }
+});
+
+// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
+const PORT = process.env.PORT;
+
+io.on('connection', (socket) => {
+  socket.on('newMessage', users=>{
+    io.emit('updateChat', users)
+  })
+
+  socket.on('acceptedFavour',()=>{
+    io.emit('updateFavours',[])
+  })
+
+  socket.on('createdFavour',()=>{
+    io.emit('updateFavours',[])
+  })
+});
+
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
